1e00c8c39d7b4acf58411af07401ed38b536e95a
unknown <>
2016-04-04 00:54:40 +0900

    160403study at home

---
+++ src/headfirst/decorator/starbuzz/Mocha.java
@@ -0,0 +1,17 @@
+package headfirst.decorator.starbuzz;
+
+public class Mocha extends CondimentDecorator {
+  Beverage beverage; //이건 왜 하는거징..?//
+  
+  public Mocha(Beverage beverage){
+    this.beverage = beverage; //.this 복습하기//
+  }
+  
+  public String getDescription() {
+    return beverage.getDescription() + ", Mocha";
+  }
+  
+  public double cost() {
+    return .20 + beverage.cost();
+  }
+}
